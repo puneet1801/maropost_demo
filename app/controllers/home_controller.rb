70ac450df0435ad6ce7c05bc8d22b19f280e7761
Ubuntu <puneetgupta18190@gmail.com>
2017-08-17 13:58:22 +0000

    send acknowledgement mail to user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def contact_form
+    UserMailer.acknowledgement_user(params).deliver_now()
     redirect_to root_url, notice: "We will contact you soon."    
   end
 
