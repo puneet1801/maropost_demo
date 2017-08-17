f05bfc69b027a15a5c34e727328ccb1e58fc6ca0
Ubuntu <puneetgupta18190@gmail.com>
2017-08-17 12:52:32 +0000

    added about_us and contact_us pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'home/index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: 'home#about_us'
+
+  get 'home/about_us'
+  get 'home/contact_us'
 end
