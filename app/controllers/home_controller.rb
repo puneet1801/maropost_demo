6d4f4d93b987565cbf940e065132dd8a5da8cfa2
Ubuntu <puneetgupta18190@gmail.com>
2017-08-18 06:50:35 +0000

    corrected spacing of code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
   def contact_form
     UserMailer.acknowledgement_user(params).deliver_now()
-    redirect_to root_url, notice: "We will contact you soon."    
+    redirect_to root_url, notice: "We will contact you soon."
   end
 
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,5 +3,5 @@ class UserMailer < ApplicationMailer
   def acknowledgement_user(data)
     @data = data
     mail(to: @data[:email], subject: "Acknowledgement Mail")
-  end  
+  end
 end
