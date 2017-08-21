f06f72ff30243dea0f848057f6b674d8e7806ead
Ubuntu <puneetgupta18190@gmail.com>
2017-08-21 09:57:42 +0000

    Add change password functionality and generate random password on signup

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,10 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def create
+  	random_password = ('a'..'z').to_a.shuffle[0,2].join+(0..9).to_a.shuffle[0,7].join+('A'..'Z').to_a.shuffle[0,1].join
+  	params[:user][:password] = random_password
+  	params[:user][:password_confirmation] = random_password
+  	super
+  end
+
+end 
